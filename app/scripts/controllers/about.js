022843715eeeb92ab9b7106bb45bd884a13558f0
Maylyn Talampas <maylynstalampas@gmail.com>
2016-06-11 17:56:26 +0800

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name taskSchedulerFeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the taskSchedulerFeApp
+ */
+angular.module('taskSchedulerFeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name taskSchedulerFeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the taskSchedulerFeApp
+ */
+angular.module('taskSchedulerFeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
